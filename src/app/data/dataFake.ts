aff70b8917ab11ca15c721e8acf40d47f18b9fab
muniquefeitoz4@gmail.com <muniquefeitoz4@gmail.com>
2023-11-14 14:09:49 -0300

    adição de novas notícias

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -7,8 +7,20 @@ export const dataFake = [
   },
   {
     "id":"2",
-    "title": "Nova Série anunciada no Disney +",
-    "description": "bla blabla",
+    "title": "Fim da 2ª temporada de Loki",
+    "description": 'O astro Tom Hiddleston falou sobre o final da segunda temporada de Loki e explicou a dinâmica do episódio. A resposta do ator, no entanto, chamou a atenção pelo tom de despedida.  “Se você ainda não viu, não darei spoilers, mas direi o seguinte: tudo se completa", explicou ele no programa The Tonight Show. "É a conclusão da segunda temporada. É também a conclusão da primeira e segunda temporadas. É também a conclusão de seis filmes, 12 episódios e 14 anos da minha vida. Eu tinha 29 anos quando fui escalado, tenho 42 agora. Foi uma jornada”, continuou. A 2ª temporada de Loki conta com o retorno de grande parte do elenco da primeira, incluindo Tom Hiddleston no papel título, Sofia Di Martino (Sylvie), Owen Wilson (Mobius), Gugu Mbatha-Raw (Ravonna) e Jonathan Majors (Kang). Entre as novidades, destaque para Ke Huy Quan (Tudo em Todo o Lugar), Rafael Casal (Ponto Cego) e Kate Dickie (A Bruxa). Com roteiro de Michael Waldron, a segunda temporada de Loki tem episódios dirigidos pela dupla Justin Benson e Aaron Moorhead (O Culto). A série está completa no Disney+.',
     "photoCover":"https://disneyplusbrasil.com.br/wp-content/uploads/2021/07/Series-Marvel-Disney-Plus-1024x576.jpg"
+  },
+  {
+    "id":"3",
+    "title": "Antes da morte de Chadwick Boseman, 'Pantera Negra 2' focaria em relação de T'Challa e seu filho",
+    "description": "A morte de Chadwick Boseman em 2020 obviamente afetou os planos para a história de 'Pantera Negra: Wakanda para sempre'. Ao perder o seu protagonista, a continuação teve de mudar o foco de sua trama, que se concentraria na relação entre o herói, T'Challa, e seu filho. A sequência do sucesso estrondoso de 2018, que arrecadou mais de US$ 1,3 bilhão em bilheterias ao redor do mundo, estreia nos cinemas brasileiros nesta quinta-feira (10). O roteiro espelha um pouco a vida real, e coloca a irmã (Letitia Wright) e a mãe (Angela Bassett) do protagonista para lidarem com as repercussões da morte do personagem, rei e protetor de uma das nações mais poderosas do mundo. 'O filme era sobre o T'Challa lidando com questões de se tornar pai, o que achávamos que era muito interessante. E quando perdemos Chadwick, o paradigma todo mudou e se tornou sobre uma mãe e uma filha', conta o produtor Nate Moore.",
+    "photoCover":"https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/35720ACD323F927C3A83D809D0F460BD3651740DA519BCC184B6F042057EA14D/scale?width=1200&aspectRatio=1.78&format=jpeg"
+  },
+  {
+    "id": "4",
+    "title": "X-Men: Fênix Negra é o segundo maior fracasso de bilheteria da história",
+    "description": "X-Men: Fênix Negra encerrou sua passagem pelos cinemas marcando como a pior arrecadação nas bilheterias da franquia dos mutantes. Segundo o Box Office Mojo, o novo capítulo dos mutantes arrecadou um total de US$ 65,8 milhões nos EUA, bem abaixo da arrecadação de Wolverine: Imortal, que acumulou US$ 132 milhões.  Na bilheteria mundial Fênix Negra também é o último colocado entre os filmes de X-Men. Ele arrecadou US$ 252 milhões, enquanto o penúltimo, o primeiro filme X-Men, de 2000, registrou um total de US$ 296 milhões. A produção teve um orçamento estimado em US$ 200 milhões, sem contar os custos de marketing e distribuição. Recentemente, de acordo com um relatório da Variety, a Disney confirmou que diminuirá o orçamento de produções da Fox, ação que pode ter ligação ao baixo desempenho de Fênix Negra ",
+    "photoCover": "https://midias.correiobraziliense.com.br/_midias/jpg/2022/07/22/675x450/1_xmen-26096415.jpeg?20220722171339?20220722171339"
   }
 ]
